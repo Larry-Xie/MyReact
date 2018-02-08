ccdc4d181363e1e35c98d4e185f7479f5d4e7d2a
Larry Xie <clisely@gmail.com>
2018-02-08 14:31:56 +0800

    Remove manifest.json and add manifest.js.

---
+++ public/manifest.js
@@ -0,0 +1,13 @@
+// Copyright © Citrix Systems, Inc.  All rights reserved.
+
+var luiManifest = {
+    consoleUrl: 'https://console-eastus-bvtrelease-a.ctxwsdev.net',
+    cwsApi: 'https://cws-eastus-bvtrelease-a.ctxwsdev.net/[customer]',
+    coreApi: 'https://core-eastus-bvtrelease-a.ctxwsdev.net/[customer]',
+    partnerJourneyApi: 'https://partner-eastus-bvtrelease-a.ctxwsdev.net/[customer]',
+    googleAnalyticsId: '',
+    geoUSServiceUri: 'https://[ctxwsservice]-us-bvtrelease-a.ctxwsdev.net',
+    geoEUServiceUri: 'https://[ctxwsservice]-eu-bvtrelease-a.ctxwsdev.net',
+    devMode: 'false',
+    testLicensingApi: 'https://licensing-eastus-bvtrelease-a.ctxwsdev.net/[customer]'
+};
