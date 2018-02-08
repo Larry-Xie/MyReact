d4daf164a364608b2959c462433c106ca00de068
Larry Xie <clisely@gmail.com>
2018-02-08 13:47:02 +0800

    Extract the styles to the top level.

--- src/views/Products/Xaxd.jsx
+++ src/views/Products/Xaxd.jsx
@@ -5,23 +5,24 @@ import { NavLink, Redirect, Route, Switch } from 'react-router-dom';
 import Status from '../Xaxd/Status';
 import Usage from '../Xaxd/Usage';
 import Users from '../Xaxd/Users';
-import './Xaxd.less';
 
 class Xaxd extends Component {
     render() {
         return (
             <React.Fragment>
-                <div className="xaxd-container">
-                        <NavLink className="xaxd-nav-item" activeClassName="xaxd-nav-active" to={`${this.props.match.url}/status`}>Server Status</NavLink>
-                        <NavLink className="xaxd-nav-item" activeClassName="xaxd-nav-active" to={`${this.props.match.url}/usage`}>Usage</NavLink>
-                        <NavLink className="xaxd-nav-item" activeClassName="xaxd-nav-active" to={`${this.props.match.url}/users`}>Users</NavLink>
+                <div className="products-nav-container">
+                    <NavLink className="products-nav-item" activeClassName="products-nav-active" to={`${this.props.match.url}/status`}>Server Status</NavLink>
+                    <NavLink className="products-nav-item" activeClassName="products-nav-active" to={`${this.props.match.url}/usage`}>Usage</NavLink>
+                    <NavLink className="products-nav-item" activeClassName="products-nav-active" to={`${this.props.match.url}/users`}>Users</NavLink>
                 </div>
-                <Switch>
-                    <Route path={`${this.props.match.url}/status`} component={Status} />
-                    <Route path={`${this.props.match.url}/usage`} component={Usage} />
-                    <Route path={`${this.props.match.url}/users`} component={Users} />
-                    <Redirect from={`${this.props.match.url}`} to={`${this.props.match.url}/status`} />
-                </Switch>
+                <div className="products-content-container">
+                    <Switch>
+                        <Route path={`${this.props.match.url}/status`} component={Status} />
+                        <Route path={`${this.props.match.url}/usage`} component={Usage} />
+                        <Route path={`${this.props.match.url}/users`} component={Users} />
+                        <Redirect from={`${this.props.match.url}`} to={`${this.props.match.url}/status`} />
+                    </Switch>
+                </div>
             </React.Fragment>
         );
     }
