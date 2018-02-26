23beaf97a3439dea388e33784906503cdad078d8
Larry Xie <clisely@gmail.com>
2018-02-26 11:08:57 +0800

    Add xaxd users nav page.

---
+++ src/components/XaxdUsers/XaxdUsers.jsx
@@ -0,0 +1,29 @@
+// Copyright © Citrix Systems, Inc.  All rights reserved.
+import React, {Component} from 'react';
+import { NavLink, Redirect, Route, Switch } from 'react-router-dom';
+
+import XaxdUsersAll from '../XaxdUsersAll/XaxdUsersAll';
+import XaxdUsersFree from '../XaxdUsersFree/XaxdUsersFree';
+import './XaxdUsers.less';
+
+class XaxdUsers extends Component {
+    render() {
+        return ( 
+            <React.Fragment>
+                <div className="users-nav-container">
+                    <NavLink className="users-nav-item" activeClassName="users-nav-active" to={`${this.props.match.url}/all`}>All Users</NavLink>
+                    <NavLink className="users-nav-item" activeClassName="users-nav-active" to={`${this.props.match.url}/free`}>Free Users List</NavLink>
+                </div>
+                <div className="users-content-container">
+                    <Switch>
+                        <Route path={`${this.props.match.url}/all`} component={XaxdUsersAll} />
+                        <Route path={`${this.props.match.url}/free`} component={XaxdUsersFree} />
+                        <Redirect from={`${this.props.match.url}`} to={`${this.props.match.url}/all`} />
+                    </Switch>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default XaxdUsers;
